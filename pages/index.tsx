54c2a924ce4090965cba863895f6daf5e62db34f
UnknownAI <ivanishyna@gmail.com>
2023-01-18 23:49:17 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+export default function Home(){
+  return (
+    <>
+      <Head>
+        <title>Test Task</title>
+        <meta name="description" content="Test Task" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={styles.main}></main>
+    </>
+  )
+}
