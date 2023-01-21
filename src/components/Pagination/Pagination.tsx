ba7aa7d537d7c09a2fe3f0741f3a97c1fb697597
UnknownAI <ivanishyna@gmail.com>
2023-01-21 21:45:11 +0100

    add pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,136 @@
+import { FC } from 'react'
+import Image from 'next/image'
+import styles from './Pagination.module.scss'
+import classNames from 'classnames'
+
+type Props = {
+  perPage: number
+  currentPage: number
+  total: number
+  onChangePage: (pageToNavigate: number) => void
+}
+
+const Pagination: FC<Props> = ({
+  perPage = 4,
+  currentPage,
+  total,
+  onChangePage,
+}) => {
+  const DOTS_MIN_LIMIT = 2
+  const MIN_PAGES_FOR_PAGINATION = 1
+  const totalPages = Math.ceil(total / perPage)
+  const startPage = 1
+  const left = 'left'
+  const right = 'right'
+
+  const hasLeftHidden = currentPage > DOTS_MIN_LIMIT
+  const hasRightHidden = totalPages - currentPage > DOTS_MIN_LIMIT
+
+  const DOTS_PAGE_OFFSET = 2
+
+  let pages: any[] = []
+
+  const generateRange = (from: number, to: number): number[] => {
+    const pagesRange = []
+
+    let current = from
+
+    while (current <= to) {
+      pagesRange.push(current)
+      current++
+    }
+
+    return pagesRange
+  }
+
+  if (totalPages < MIN_PAGES_FOR_PAGINATION) {
+    return null
+  }
+
+  if (hasLeftHidden && hasRightHidden) {
+    pages = [
+      startPage,
+      left,
+      ...generateRange(currentPage - 1, currentPage + 1),
+      right,
+      totalPages,
+    ]
+  }
+
+  if (hasRightHidden && !hasLeftHidden && pages.length === 0) {
+    pages = [...generateRange(startPage, DOTS_MIN_LIMIT + 1), right, totalPages]
+  }
+
+  if (!hasRightHidden && hasLeftHidden && pages.length === 0) {
+    pages = [startPage, left, ...generateRange(currentPage - 1, totalPages)]
+  }
+
+  if (pages.length === 0) {
+    pages = generateRange(startPage, totalPages)
+  }
+
+  return (
+    <ul className={styles.paginationList}>
+      {currentPage > 1 && (
+        <li className={styles.paginationItem}>
+          <button
+            className={classNames(
+              styles.paginationItemButton,
+              styles.arrowLeft,
+            )}
+            onClick={() => {
+              onChangePage(currentPage - 1)
+            }}
+          >
+            <Image src="./Arrow.svg" alt="arrow" width={15} height={15} />
+          </button>
+        </li>
+      )}
+      {pages.map((item) => {
+        let pageToNavigate = item
+        let element = item
+        const itemClass = classNames([styles.paginationItem], {
+          [styles.activeItem]: currentPage === item,
+        })
+
+        if (item === 'left') {
+          element = '...'
+          pageToNavigate = currentPage - DOTS_PAGE_OFFSET
+        }
+
+        if (item === 'right') {
+          element = '...'
+          pageToNavigate = currentPage + DOTS_PAGE_OFFSET
+        }
+
+        return (
+          <li className={itemClass} key={item}>
+            <button
+              onClick={() => {
+                onChangePage(pageToNavigate)
+              }}
+              className={styles.paginationItemButton}
+              disabled={currentPage === item}
+            >
+              {!isNaN(element) ? parseInt(element) : element}
+            </button>
+          </li>
+        )
+      })}
+      {currentPage !== total && (
+        <li className={styles.paginationItem}>
+          <button
+            className={styles.paginationItemButton}
+            onClick={() => {
+              onChangePage(currentPage + 1)
+            }}
+          >
+            <Image src="./Arrow.svg" alt="arrow" width={15} height={15} />
+          </button>
+        </li>
+      )}
+    </ul>
+  )
+}
+
+export default Pagination
--- src/pages/Experts/Experts.tsx
+++ src/pages/Experts/Experts.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { GetServerSideProps } from 'next'
 import classNames from 'classnames'
 import { User } from '@/src/types/User'
 import ExpertCard from '@/src/components/ExpertCard'
 import styles from './Experts.module.scss'
+import Pagination from '@/src/components/Pagination'
 
 interface Props {
   experts: User[]
@@ -11,6 +12,12 @@ interface Props {
 }
 
 const Experts: FC<Props> = ({ total, experts }) => {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const handleChangePage = (page: number): void => {
+    setCurrentPage(page)
+  }
+
   return (
     <div className={classNames('content-container', styles.main)}>
       <h1 className="h1">Find your Experts</h1>
@@ -23,6 +30,13 @@ const Experts: FC<Props> = ({ total, experts }) => {
             </li>
           ))}
         </ul>
+      </div>
+      <div className={styles.pagination}>
+        <Pagination
+          currentPage={currentPage}
+          total={total}
+          onChangePage={handleChangePage}
+        />
       </div>
     </div>
   )
